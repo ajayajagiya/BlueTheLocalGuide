2e7da0db009aeb3c4e39bbec15e2f810c3f85149
Ajay Ajagiya <ajagiyaajayk@gmail.com>
2025-08-30 17:36:24 +0530

    Update virtual tours and street view data with new locations and iframe sources

--- Images360Data.js
+++ Images360Data.js
@@ -6,27 +6,39 @@
 
 const images360Data = [
   {
-    title: "Retro Bistro : The Fine Multi Cuisine Restaurant.",
-    iframeSrc: "https://www.google.com/maps/embed?pb=!4v1649231962201!6m8!1m7!1sCAoSLEFGMVFpcFBJTnJtR19BVkpHYmE4Y0tST1o0aExqUTZhVERuUS0waTlTa3FH!2m2!1d22.3078204!2d73.16408779999999!3f211.87789790992286!4f-6.864679630696017!5f0.4000000000000002"
+    title: "Nilkanthdham Poicha",
+    iframeSrc: "https://www.google.com/maps/embed?pb=!4v1641972343641!6m8!1m7!1sCAoSLEFGMVFpcE51eUt5Mi1uWkdMQVYxdGRhVW5rWGxiZzZZVkFQUDZPSXozcmh1!2m2!1d21.9705292!2d73.47147129999999!3f0!4f0!5f0.4000000000000002"
   },
   {
-    title: "Siddharth Farm, Vadodara.",
-    iframeSrc: "https://www.google.com/maps/embed?pb=!4v1648474719389!6m8!1m7!1sCAoSLEFGMVFpcE9BMmJ2Z3E5elRZek52RFJOVVJkN0w5VFRFcVlZYnlHekl0VTJo!2m2!1d22.3626298!2d73.20739787!3f112.03559079560524!4f-5.685190385168411!5f0.4000000000000002"
+    title: "Statue Of Unity",
+    iframeSrc: "https://www.google.com/maps/embed?pb=!4v1641974276629!6m8!1m7!1sCAoSLEFGMVFpcFA5djEzR19JLU5fWkZudElxWWxCRUJmR0p0RjNZeU12aTJqMDlW!2m2!1d21.83794406814737!2d73.7192789465189!3f278.8923392391219!4f2.2133704859936785!5f0.4000000000000002"
   },
   {
-    title: "Global Antico Process Systems Pvt. Ltd.",
-    iframeSrc: "https://www.google.com/maps/embed?pb=!4v1694839168159!6m8!1m7!1sCAoSLEFGMVFpcE95c2J0ak9TaDJPdDlxWHNtaVZxR1JYT1NhVUFWT0dQYmR4am1n!2m2!1d22.31608866!2d73.23627311!3f114.94152405536927!4f-9.021038188763285!5f0.4000000000000002"
+    title: "Nilkanthdham Poicha",
+    iframeSrc: "https://www.google.com/maps/embed?pb=!4v1641971214974!6m8!1m7!1sCAoSLEFGMVFpcE04YXhKdkF0YWpqclA0cDZCMER2QnlURnlYTDFNb2lLOFZUanZB!2m2!1d21.9705292!2d73.47147129999999!3f3.2142857142857153!4f2.1515679442508713!5f0.4000000000000002"
   },
   {
-    title: "Shree Pramukh Transport Co, Vadodara.",
-    iframeSrc: "https://www.google.com/maps/embed?pb=!4v1677983993339!6m8!1m7!1sCAoSLEFGMVFpcFBEVWhZVjBQNm1TY0dBejl3cFhLUmpUdWRTZ0JSSHVCWGNJaXhm!2m2!1d22.30017225!2d73.19409569!3f178.72904706698358!4f-2.0863811867862836!5f0.4000000000000002"
+    title: "Nilkanthdham Poicha",
+    iframeSrc: "https://www.google.com/maps/embed?pb=!4v1641972242490!6m8!1m7!1sCAoSLEFGMVFpcE9td3FGdy1NalVYdHFaV2huRWx2djIyQS1WZHh3Y1ozaW45V2hr!2m2!1d21.9705292!2d73.47147129999999!3f79.8396869987921!4f22.63923206532327!5f0.4000000000000002"
   },
   {
-    title: "Flavoury bakers, Surat.",
-    iframeSrc: "https://www.google.com/maps/embed?pb=!4v1673148256691!6m8!1m7!1sCAoSLEFGMVFpcE00ZTlvaHRkSTViRUQ4SHU5R3FuVWZFT0R3cTNXcGh0M2xVSXVB!2m2!1d21.24164494!2d72.85255061!3f324.40489228659084!4f-2.239458831367571!5f0.4000000000000002"
+    title: "Dmart Vadodara.",
+    iframeSrc: "https://www.google.com/maps/embed?pb=!4v1641973557955!6m8!1m7!1sCAoSLEFGMVFpcE1OSDJiOHFMQ1pDUlFVUjhlQVhIUkFrVGtydFFDVXlEd2lZZFRQ!2m2!1d22.32557315927438!2d73.17622889503686!3f229.8367413354402!4f5.174741317799999!5f0.4000000000000002"
   },
   {
-    title: "Jagrut Hanuman Temple, Vadodara.",
-    iframeSrc: "https://www.google.com/maps/embed?pb=!4v1683824556656!6m8!1m7!1sCAoSLEFGMVFpcE1WU1kzeGR2X1VycENsQ2J6RUVLY2pVQUdsdW9ZRXZDYXlVVVhK!2m2!1d22.28992274!2d73.17030684!3f24.925074692909668!4f6.634923952168762!5f0.4000000000000002"
+    title: "Vadodara.",
+    iframeSrc: "https://www.google.com/maps/embed?pb=!4v1641973615358!6m8!1m7!1sCAoSLEFGMVFpcFB5RkRJNnFxSlhYSkxKd1hFWjQwb0dEX0hVcVV2TWNzVDNWQi1W!2m2!1d22.2900314!2d73.3641281!3f83.81030366344504!4f3.648820660993991!5f0.4000000000000002"
+  },
+  {
+    title: "Nilkanthdham Poicha",
+    iframeSrc: "https://www.google.com/maps/embed?pb=!4v1641972119563!6m8!1m7!1sCAoSLEFGMVFpcE0xRWhFQVZuT2V3NXBJNVVrdkVGNExfRVRmcWpiN25iNzFsaXJZ!2m2!1d21.9705292!2d73.47147129999999!3f-0.30487804878048785!4f-1.0365853658536537!5f0.4000000000000002"
+  },
+  {
+    title: "Allscripts India LLC, Vadodara.",
+    iframeSrc: "https://www.google.com/maps/embed?pb=!4v1641974059007!6m8!1m7!1sCAoSLEFGMVFpcE5FUmR2RjhYOGYzSnZyeWkzX0NkOXVSNjFLMXYyU1JfcmlkRjBE!2m2!1d22.3165421!2d73.1674936!3f320.242008239309!4f28.06164151867526!5f0.4000000000000002"
+  },
+  {
+    title: "Baroda Museum & Picture Gallery",
+    iframeSrc: "https://www.google.com/maps/embed?pb=!4v1641973444227!6m8!1m7!1sCAoSLEFGMVFpcE9Sb2Y3Zy01M1lZbC1Tdmo1Sm9XMkk3dGhncHdvdFE2S1dCY3ZY!2m2!1d22.30715883886672!2d73.18121873632892!3f270!4f0!5f0.4000000000000002"
   }
 ];
--- StreetViewData.js
+++ StreetViewData.js
@@ -1,26 +1,18 @@
 const streetViewData = [
   {
-    title: "Retro Bistro : The Fine Multi Cuisine Restaurant.",
-    iframeSrc: "https://www.google.com/maps/embed?pb=!4v1649231962201!6m8!1m7!1sCAoSLEFGMVFpcFBJTnJtR19BVkpHYmE4Y0tST1o0aExqUTZhVERuUS0waTlTa3FH!2m2!1d22.3078204!2d73.16408779999999!3f211.87789790992286!4f-6.864679630696017!5f0.4000000000000002"
+    title: "Vadodara Central, Vadodara",
+    iframeSrc: "https://www.google.com/maps/embed?pb=!4v1646032007959!6m8!1m7!1sCAoSLEFGMVFpcE53SGtzWllpYUtNTEw0dE0wSUpJVVhwbjNSN09FSDJPaDVRR1V6!2m2!1d22.317280091634!2d73.169528669649!3f90!4f0!5f0.4000000000000002"
   },
   {
-    title: "Siddharth Farm, Vadodara.",
-    iframeSrc: "https://www.google.com/maps/embed?pb=!4v1648474719389!6m8!1m7!1sCAoSLEFGMVFpcE9BMmJ2Z3E5elRZek52RFJOVVJkN0w5VFRFcVlZYnlHekl0VTJo!2m2!1d22.3626298!2d73.20739787!3f112.03559079560524!4f-5.685190385168411!5f0.4000000000000002"
+    title: "Atlantis Building, Vadodara",
+    iframeSrc: "https://www.google.com/maps/embed?pb=!4v1646031083631!6m8!1m7!1sCAoSLEFGMVFpcE05R0tqMW9OLWpYSDhOQkJESVZucjRLYVdmMkpBUWlzVHlYbUhV!2m2!1d22.317335653929!2d73.167350375738!3f47.39642298286674!4f17.566444998070295!5f0.4000000000000002"
   },
   {
-    title: "Global Antico Process Systems Pvt. Ltd.",
-    iframeSrc: "https://www.google.com/maps/embed?pb=!4v1694839168159!6m8!1m7!1sCAoSLEFGMVFpcE95c2J0ak9TaDJPdDlxWHNtaVZxR1JYT1NhVUFWT0dQYmR4am1n!2m2!1d22.31608866!2d73.23627311!3f114.94152405536927!4f-9.021038188763285!5f0.4000000000000002"
+    title: "Atlantis Building, Vadodara",
+    iframeSrc: "https://www.google.com/maps/embed?pb=!4v1646032219439!6m8!1m7!1sCAoSLEFGMVFpcE5NQnRDUWYxUXhKWXFkdDJiOXFSMVV1eGVXYzhyMmhYWTVSLWtF!2m2!1d22.317218788256!2d73.167590027751!3f315.93310277093303!4f-2.8522631979943895!5f0.4000000000000002"
   },
   {
-    title: "Shree Pramukh Transport Co, Vadodara.",
-    iframeSrc: "https://www.google.com/maps/embed?pb=!4v1677983993339!6m8!1m7!1sCAoSLEFGMVFpcFBEVWhZVjBQNm1TY0dBejl3cFhLUmpUdWRTZ0JSSHVCWGNJaXhm!2m2!1d22.30017225!2d73.19409569!3f178.72904706698358!4f-2.0863811867862836!5f0.4000000000000002"
-  },
-  {
-    title: "Flavoury bakers, Surat.",
-    iframeSrc: "https://www.google.com/maps/embed?pb=!4v1673148256691!6m8!1m7!1sCAoSLEFGMVFpcE00ZTlvaHRkSTViRUQ4SHU5R3FuVWZFT0R3cTNXcGh0M2xVSXVB!2m2!1d21.24164494!2d72.85255061!3f324.40489228659084!4f-2.239458831367571!5f0.4000000000000002"
-  },
-  {
-    title: "Jagrut Hanuman Temple, Vadodara.",
-    iframeSrc: "https://www.google.com/maps/embed?pb=!4v1683824556656!6m8!1m7!1sCAoSLEFGMVFpcE1WU1kzeGR2X1VycENsQ2J6RUVLY2pVQUdsdW9ZRXZDYXlVVVhK!2m2!1d22.28992274!2d73.17030684!3f24.925074692909668!4f6.634923952168762!5f0.4000000000000002"
+    title: "Amit Nagar, Vadodara",
+    iframeSrc: "https://www.google.com/maps/embed?pb=!4v1646200599539!6m8!1m7!1sCAoSLEFGMVFpcE1LbUZkRlpQUm9kUGZ4N1YyRkhlOW1yVzZEX0tDQ3h1N3lRS2Ft!2m2!1d22.32732703!2d73.20611402!3f333.83872041004656!4f4.3223575396048375!5f0.4000000000000002"
   }
 ];
